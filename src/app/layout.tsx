4ec61738e29e2dbeefd1132cdf8446abc2437cdc
Admin <admin@wegrow.com>
2026-03-16 10:51:15 -0300

    WE9.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css"; 
 import { AuthProvider } from '@/lib/contexts/AuthContext';
 import LayoutWrapper from '../components/layout-wrapper'; 
+
+// 👇 AS DUAS ANTENAS DO VERCEL (Analytics e Speed Insights)
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -45,8 +48,11 @@ export default function RootLayout({
           </LayoutWrapper>
         </AuthProvider>
         
-        {/* 👇 A ANTENA DO VERCEL INJETADA AQUI 👇 */}
+        {/* 👇 RASTREADOR DE VISITANTES */}
         <Analytics />
+        
+        {/* 👇 RASTREADOR DE VELOCIDADE E PERFORMANCE */}
+        <SpeedInsights />
         
       </body>
     </html>
